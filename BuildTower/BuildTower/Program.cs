5ca99fccf0ab104b28e6a2693ecb0433890382c7
Konrad Lacek <konrad.lacek@outlook.com>
2025-08-05 21:10:36 +0200

    Add project files.

---
+++ BuildTower/BuildTower/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+
+namespace BuildTower
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// A method drawing a pyramid tower based on the provided number of floors.
+        /// </summary>
+        /// <param name="nFloors"></param>
+        /// <returns></returns>
+        internal static string[] TowerBuilder(int nFloors)
+        {
+            if(nFloors == 0)
+            {
+                return [];
+            }
+            int lowestFloorLength = 2 * (nFloors - 1) + 1;
+            int middleIndex = lowestFloorLength / 2;
+            string[] drawnFloors = new string[nFloors];
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < nFloors; i++)
+            {
+                int currentFloorAsterisk = 2 * i + 1;
+                for (int j = 0; j < lowestFloorLength; j++)
+                {
+                    if (j < (middleIndex - currentFloorAsterisk / 2) || j > (middleIndex + currentFloorAsterisk / 2))
+                    {
+                        sb.Append(" ");
+                    }
+                    else
+                    {
+                        sb.Append("*");
+                    }
+                }
+                drawnFloors[i] = sb.ToString();
+                sb.Clear();
+            }
+            return drawnFloors;
+        }
+
+        /// <summary>
+        /// A method displaying TowerBuilder() tower
+        /// </summary>
+        /// <param name="tower"></param>
+        internal static void DisplayTower(string[] tower)
+        {
+            foreach (string i in tower)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ConsoleColor originalColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Code Wars\nBuild Tower\n6 kyu\n");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("No Main implemented, nothing to see here!");
+            Console.ForegroundColor= originalColor;
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BuildTower/BuildTowerTests/TowerTests.cs
@@ -0,0 +1,26 @@
+﻿using static BuildTower.Program;
+
+namespace BuildTowerTests
+{
+    [TestClass]
+    public sealed class TowerTests
+    {
+        [TestMethod]
+        public void OneFloor()
+        {
+            CollectionAssert.AreEqual(TowerBuilder(1), new[] {"*"});
+        }
+
+        [TestMethod]
+        public void ThreeFloors()
+        {
+            CollectionAssert.AreEqual(TowerBuilder(3), new[] { "  *  ", " *** ", "*****"});
+        }
+
+        [TestMethod]
+        public void FiveFloors()
+        {
+            CollectionAssert.AreEqual(TowerBuilder(5), new[] { "    *    ", "   ***   ", "  *****  ", " ******* ", "*********"});
+        }
+    }
+}
