1233ddb21f5ede15375fd5215bc05488db39fe84
Konrad Lacek <konrad.lacek@outlook.com>
2025-08-06 18:58:56 +0200

    Code Wars Detect Pangram kata, 6 kyu + Appropriate Tests

---
+++ DetectPangram/DetectPangram/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace DetectPangram
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Detects if a give string is a pangram, so if it contains all characters of the standard English alphabet.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        internal static bool IsPangram(string s)
+        {
+            if (s.Length < 26 || string.IsNullOrWhiteSpace(s))
+            {
+                return false;
+            }
+            // Can check it all without checking for both lower and upper case, with just getting the s either to upper or to lower case, the intent was different, though.
+            char[] alphabetTable = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            List<char> dividedString = s.ToCharArray().ToList();
+            for (int i = 0; i < alphabetTable.Length; i++)
+            {
+                if (dividedString.Contains(alphabetTable[i]) || dividedString.Contains(alphabetTable[i].ToString().ToUpper().ToCharArray()[0]))
+                {
+                    continue;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            ConsoleColor originalColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Code Wars\nDetect Pangram\n6 kyu\n");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("No Main implemented, nothing to see here!");
+            Console.ForegroundColor = originalColor;
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DetectPangram/DetectPangramTests/PangramDetectionTests.cs
@@ -0,0 +1,28 @@
+﻿using static DetectPangram.Program;
+
+namespace DetectPangramTests
+{
+    [TestClass]
+    public sealed class PangramDetectionTests
+    {
+
+
+        [TestMethod]
+        public void PangramTest1()
+        {
+            Assert.IsTrue(IsPangram("The quick brown fox jumps over the lazy dog."));
+        }
+
+        [TestMethod]
+        public void PangramTest2()
+        {
+            Assert.IsTrue(IsPangram("abcdefghijklmnopqrstuvwxyz"));
+        }
+
+        [TestMethod]
+        public void PangramTest3()
+        {
+            Assert.IsFalse(IsPangram("cnscndcscidnbcisdbisducbdsicuhbdichdbcibsichdbibdciudbcidsuhbcdiuhcbdidcubhscicdbdiucbsib"));
+        }
+    }
+}
