3a068ba678021c2cb4c64d0678a8f4c1e6232269
Konrad Lacek <konrad.lacek@outlook.com>
2025-08-07 21:21:41 +0200

    Code Wars Find Unique kata, 6 kyu + Appropriate Tests

---
+++ FindUnique/FindUnique/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace FindUnique
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Find a unique int in a collection with only one unique value.
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        internal static int GetUnique(IEnumerable<int> numbers)
+        {
+            List<int> numberList = numbers.ToList();
+            if (numberList.Count < 3)
+            {
+                return 0;
+            }
+            for(int i = 1; i < numberList.Count - 1; i++)
+            {
+                if (numberList[i] != numberList[i - 1] && numberList[i] != numberList[i + 1])
+                {
+                    return numberList[i];
+                }
+            }
+            if (numberList[0] != numberList[1])
+            {
+                return numberList[0];
+            }
+            if (numberList[numberList.Count-1] != numberList[numberList.Count - 2])
+            {
+                return numberList[numberList.Count-1];
+            }
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> testList = [8, 8, 8, 8, 7];
+            Console.WriteLine(GetUnique((testList)));
+            ConsoleColor originalColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Code Wars\nFind Unique\n6 kyu\n");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("No Main implemented, nothing to see here!");
+            Console.ForegroundColor = originalColor;
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FindUnique/FindUniqueTests/GetUniqueTests.cs
@@ -0,0 +1,36 @@
+﻿using static FindUnique.Program;
+
+namespace FindUniqueTests
+{
+    [TestClass]
+    public sealed class GetUniqueTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            List<int> testList = [1, 2, 2, 2];
+            Assert.AreEqual(1, GetUnique(testList));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            List<int> testList = [-2, 2, 2, 2];
+            Assert.AreEqual(-2, GetUnique(testList));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            List<int> testList = [11, 11, 14, 11, 11];
+            Assert.AreEqual(14, GetUnique(testList));
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            List<int> testList = [8, 8, 8, 8, 7];
+            Assert.AreEqual(7, GetUnique(testList));
+        }
+    }
+}
